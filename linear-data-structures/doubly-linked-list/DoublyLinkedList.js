d4d192e44f5831a3448de7ab5ed2c18bf70a7550
Víctor Moreno <victorgmp@gmail.com>
2020-11-10 17:02:46 -0400

    add to head & tail methods

--- linear-data-structures/doubly-linked-list/DoublyLinkedList.js
+++ linear-data-structures/doubly-linked-list/DoublyLinkedList.js
@@ -10,6 +10,29 @@ class DoublyLinkedList {
   addToHead(data) {
     const newHead = new Node(data);
     const currentHead = this.head;
+
+    if (currentHead) {
+      currentHead.setPreviousNode(newHead);
+      newHead.setNextNode(currentHead);
+    }
+
+    this.head = newHead;
+
+    if (!this.tail) this.tail = newHead;
+  }
+
+  addToTail(data) {
+    const newTail = new Node(data);
+    const currentTail = this.tail;    
+
+    if (currentTail) {
+      currentTail.setNextNode(newTail);
+      newTail.setPreviousNode(currentTail);
+    }
+
+    this.tail = newTail;
+
+    if (!this.head) this.head = newTail;
   }
   
   printList() {
