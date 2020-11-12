22a09e3227d068a54c4a66450dd618f12f23629f
Víctor Moreno <victorgmp@gmail.com>
2020-11-12 17:43:04 -0400

    add hasRoom & isEmpty methods

--- linear-data-structures/04-queues/Queue.js
+++ linear-data-structures/04-queues/Queue.js
@@ -1,9 +1,24 @@
 const LinkedList = require('./LinkedList');
 
 class Queue {
-  constructor() {
+  constructor(maxSize = Infinity) {
     this.queue = new LinkedList();
     this.size = 0;
+    this.maxSize = maxSize;
+  }
+
+  hasRoom() {
+    if (this.size < this.maxSize) {
+      return true;
+    }
+    return false;
+  }
+
+  isEmpty() {
+    if (this.size === 0) {
+      return true;
+    }
+    return false;
   }
 
   enqueue(data) {
@@ -22,12 +37,12 @@ class Queue {
 
 module.exports = Queue;
 
-const restaurantOrder = new Queue();
-restaurantOrder.enqueue('apple pie');
-restaurantOrder.enqueue('roast chicken');
-restaurantOrder.enqueue('quinoa salad');
-console.log('\nFood preparing...\n')
-restaurantOrder.dequeue();
-restaurantOrder.dequeue();
-restaurantOrder.dequeue();
-console.log('All orders ready!')
+// const restaurantOrder = new Queue();
+// restaurantOrder.enqueue('apple pie');
+// restaurantOrder.enqueue('roast chicken');
+// restaurantOrder.enqueue('quinoa salad');
+// console.log('\nFood preparing...\n')
+// restaurantOrder.dequeue();
+// restaurantOrder.dequeue();
+// restaurantOrder.dequeue();
+// console.log('All orders ready!')
