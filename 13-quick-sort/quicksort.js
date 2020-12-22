166dda05d2a5ac57f12636b25c25b464bd9d1268
Víctor Moreno <victorgmp@gmail.com>
2020-12-22 11:21:52 -0400

    fix lint errorrs

--- 13-quick-sort/quicksort.js
+++ 13-quick-sort/quicksort.js
@@ -11,8 +11,7 @@ const quicksort = (array, leftBound = 0, rightBound = array.length - 1) => {
     quicksort(array, pivotIndex, rightBound);
   }
   return array;
-}
-
+};
 
 const partition = (array, leftIndex, rightIndex) => {
   const pivot = array[Math.floor((rightIndex + leftIndex) / 2)];
@@ -20,11 +19,11 @@ const partition = (array, leftIndex, rightIndex) => {
   while (leftIndex <= rightIndex) {
     while (array[leftIndex] < pivot) {
       leftIndex++;
-      console.log(`.. ${array[leftIndex-1]} < ${pivot} : Incremented leftIndex => ${leftIndex}`);
+      console.log(`.. ${array[leftIndex - 1]} < ${pivot} : Incremented leftIndex => ${leftIndex}`);
     }
     while (array[rightIndex] > pivot) {
       rightIndex--;
-      console.log(`.. ${array[rightIndex+1]} > ${pivot} : Decremented rightIndex => ${rightIndex}`);
+      console.log(`.. ${array[rightIndex + 1]} > ${pivot} : Decremented rightIndex => ${rightIndex}`);
     }
     if (leftIndex <= rightIndex) {
       const string = `${leftIndex} <= ${rightIndex}`;
@@ -36,9 +35,9 @@ const partition = (array, leftIndex, rightIndex) => {
     }
   }
   return leftIndex;
-}
+};
 
 module.exports = {
   quicksort,
-  partition
+  partition,
 };
